bf7016d960e2459ce446eb4da7c30f70daab2c03
Gonçalo Almeida <48161282+Gonkalos@users.noreply.github.com>
2020-12-21 00:08:57 +0000

    Organize files

---
+++ Classes/Class 7/executable.sh
@@ -0,0 +1,11 @@
+# Clone BMStub
+git clone https://github.com/jopereira/bmstub.git
+
+# Download Apache JMeter (Binaries)
+wget https://mirrors.up.pt/pub/apache//jmeter/binaries/apache-jmeter-5.4.tgz
+
+# Unpack Apache JMeter
+tar -xvf apache-jmeter-5.4.tgz
+
+# Files
+rm apache-jmeter-5.4.tgz
